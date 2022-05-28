738dab285c4f949c39450b6dd73d07e83f4c6546
HDuCoing <hollyducoing@gmail.com>
2022-05-28 12:28:08 +1200

    updated - table building

--- flaskflights/models.py
+++ flaskflights/models.py
@@ -1,14 +1,5 @@
-from sqlalchemy import func
-from sqlalchemy.orm import session
-from flaskflights.forms import FlightSelect, RegistrationForm, LoginForm
-
 from flaskflights import db
 
-aircrafts = ['SyberJet SJ30i',
-             'Cirrus SF50 1',
-             'Cirrus SF50 2',
-             'HondaJet Elite 1',
-             'HondaJet Elite 2']
 
 # user details
 class User(db.Model):
@@ -20,24 +11,16 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}')"
-
-# flight booking dates to be returned when you search for a flight
-class BookingDates(db.Model):
-    __tablename__ = 'bookingdates'
-    id = db.Column(db.Integer, primary_key=True)
-    leaveOn = db.Column(db.Integer, nullable=False)
-    returnOn = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"('{self.leaveOn}','{self.returnOn}')"
-
 # flight booking info for user's account info page
 class Booking(db.Model):
     __tablename__ = 'booking'
     id = db.Column(db.Integer, primary_key=True)
+    # user ID
+    user = db.Column(db.Integer, nullable=False)
     bookingRef = db.Column(db.String(100), nullable=False)
     flightDates = db.Column(db.Integer, nullable=False)
     booking_id = db.Column(db.Integer, db.ForeignKey('booking.id'), nullable=False)
+    aircraft = db.Column(db.String)
 
     def __repr__(self):
         return f"Booking('{self.bookingRef}', '{self.flightDates}')"
@@ -51,15 +34,4 @@ class Aircraft(db.Model):
 
     def __repr__(self):
         return f"Aircraft('{self.model}','{self.capacity}')"
-
-class AvailableFlights(db.Model):
-    __tablename__ = 'available_flights'
-    id = db.Column(db.Integer, primary_key=True)
-    # date equals flight select form output
-    dayOfFlight = db.Column(db.String, nullable=False)
-    aircrafts = db.Column(db.String, nullable=False)
-    seatsAvailable = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"AvailableFlights('{self.date}', '{self.time}')"
 
--- flaskflights/routes.py
+++ flaskflights/routes.py
@@ -25,7 +25,7 @@ def register():
         db.session.add(user)
         flash('Thanks for registering with AirDuCoing!')
         return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+    return render_template('register.html', form=form, title="Register")
 
 #todo make search work
 @app.route('/book', methods=['GET', 'POST'])
@@ -41,15 +41,48 @@ def book():
         # day of week - monday is 0 - sunday is 6
         dayLeave = leave.weekday()
         dayReturn = return_on.weekday()
+        chosenDateRange = []
+        aircrafts = ['SyberJet SJ30i',
+                     'Cirrus SF50 1',
+                     'Cirrus SF50 2',
+                     'HondaJet Elite 1',
+                     'HondaJet Elite 2']
+        # create a range of weekdays for flight search
+        for i in range(dayLeave, dayReturn):
+            chosenDateRange.append(i)
 
-        if dayLeave == 0: # if monday
-            #if flyFrom == 'Dairy Flat':
-            flights_available = [
-                '8:00 A.M. Dairy Flat to Rotorua',
-                '12 P.M. Rotorua to Dairy Flat',
-            ]
+        if location == 'Dairy Flat':
+            # friday morning - to rotorua then sydney - syberjet
 
-        print("Location: " + location)
-        print("Leave: " + str(dayLeave) + " Return: " + str(dayReturn))
+            # monday through friday; - cirrus jets
+            # morning - to rotorua
+            # late afternoon - to rotorua
+
+            # tuesday and friday - to tuuta - hondajets
+            # monday - to tekapo - hondajets
+            # monday wednesday friday - morning - to great barrier island - cirrus jets
+
+            if range(0, 4) in chosenDateRange: # monday to friday
+                # show flights
+            #todo
+        elif location == 'Rotorua':
+            # friday morning - to sydney - syberjet
+
+            # monday through friday; - cirrus jets
+            # noon - to dairy flat
+            # evening - to dairy flat
+            #todo
+        elif location == 'Tuuta':
+            # wednesday and saturday - to dairy flat - HondaJets
+            #todo
+        elif location == 'Sydney':
+            # sunday afternoon - to dairy flat - syberjet
+            #todo
+        elif location == 'Tekapo':
+            # friday - to dairy flat - hondajet
+            #todo
+        elif location == 'Great Barrier Island':
+            # monday, friday, saturday - to dairy flat - cirrus jets
+            #todo
         return render_template('flight_info.html')
-    return render_template('book.html', form=form)
+    return render_template('book.html', form=form, title="Book")
